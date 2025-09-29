e842ec7244d72b002ecd168ba262d444bd759d46
IsraelJorge <israeljorge2000@gmail.com>
2025-09-29 15:11:13 -0300

    refactor: integrate validation class for user registration and streamline validation logic

---
+++ Validation.php
@@ -0,0 +1,81 @@
+<?php
+
+class Validation
+{
+  public $validations;
+
+  public function __construct()
+  {
+    $this->validations = [];
+  }
+
+  public static function validate($rules, $data)
+  {
+    $validation = new self;
+
+    foreach ($rules as $field => $fieldRules) {
+      $nameField = $fieldRules['fieldName'] ?? $field;
+      $rulesForField = $fieldRules['rules'] ?? [];
+
+      foreach ($rulesForField as $rule) {
+        $fieldValue = $data[$field] ?? '';
+
+        if ($rule === 'confirmed') {
+          $confirmKey = "{$field}_confirm";
+          $confirmValue = $data[$confirmKey] ?? null;
+          $validation->$rule($nameField, $fieldValue, $confirmValue);
+        } elseif (str_contains($rule, ':')) {
+          $temp = explode(':', $rule, 2);
+          $ruleMinOrMax = $temp[0];
+          $ruleArg = $temp[1];
+
+          $validation->$ruleMinOrMax($nameField, $fieldValue, $ruleArg);
+        } else {
+          $validation->$rule($nameField, $fieldValue);
+        }
+      }
+    }
+
+    return $validation;
+  }
+
+  private function required($field, $value)
+  {
+    if (strlen($value) == 0) {
+      $this->validations[] = "O $field é obrigatório.";
+    }
+  }
+
+  private function email($field, $value)
+  {
+    if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+      $this->validations[] = "O $field é inválido.";
+    }
+  }
+
+  private function confirmed($field, $value, $valueConfirm)
+  {
+    if ($value != $valueConfirm) {
+      $this->validations[] = "O $field de confirmação está diferente.";
+    }
+  }
+
+  private function min($field, $value, $min)
+  {
+    if (strlen($value) < $min) {
+      $this->validations[] = "O $field precisa ter no mínimo $min caracteres.";
+    }
+  }
+
+  private function max($field, $value, $max)
+  {
+    if (strlen($value) > $max) {
+      $this->validations[] = "O $field precisa ter no máximo $max caracteres.";
+    }
+  }
+
+  public function isInvalid()
+  {
+    return sizeof($this->validations) > 0;
+  }
+}
--- controllers/registration.controller.php
+++ controllers/registration.controller.php
@@ -1,52 +1,46 @@
 <?php
+require 'Validation.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $validations = [];
-
   $name = trim($_POST['name']);
   $email = trim($_POST['email']);
   $email_confirm = trim($_POST['email_confirm']);
   $password = trim($_POST['password']);
 
-  if (strlen($name) == 0) {
-    $validations[] = 'O nome é obrigatório.';
-  }
+  $validation = Validation::validate([
+    'name' => [
+      'fieldName' => 'Nome',
+      'rules' => ['required']
+    ],
+    'email' => [
+      'fieldName' => 'E-mail',
+      'rules' => ['required', 'email', 'confirmed']
+    ],
+    'password' => [
+      'fieldName' => 'Senha',
+      'rules' => ['required', 'min:8', 'max:30']
+    ]
+  ], [
+    'name' => $name,
+    'email' => $email,
+    'email_confirm' => $email_confirm,
+    'password' => $password
+  ]);
 
-  if (strlen($email) == 0) {
-    $validations[] = 'O e-mail é obrigatório.';
-  }
-
-  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $validations[] = 'O e-mail é inválido.';
-  }
-
-  if ($email != $email_confirm) {
-    $validations[] = 'O e-mail de confirmação está diferente.';
-  }
-
-  if (strlen($password) == 0) {
-    $validations[] = 'O senha é obrigatória.';
-  }
-
-  if (strlen($password) < 8 || strlen($password) > 30) {
-    $validations[] = 'A senha precisa ter entre 8 e 30 caracteres.';
-  }
-
-  if (sizeof($validations) > 0) {
-    $_SESSION['validations'] = $validations;
+  if ($validation->isInvalid()) {
+    $_SESSION['validations'] = $validation->validations;
     header('location: /login');
     exit();
   }
 
   $_SESSION['validations'] = null;
 
-
   (new DB())->query(
     query: 'INSERT INTO users (name, email, password) VALUES (:name, :email, :password)',
     params: [
-      'name' => $_POST['name'],
-      'email' => $_POST['email'],
-      'password' => $_POST['password']
+      'name' => $name,
+      'email' => $email,
+      'password' => password_hash($password, PASSWORD_DEFAULT)
     ]
   );
 }
